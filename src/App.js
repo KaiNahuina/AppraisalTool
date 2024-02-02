dd07380c7d7376fe73ad3bf3741147ae430de500
KaiNahuina <knahuina.code@gmail.com>
2024-02-02 01:40:08 +0000

    Layout Progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Router>
+          <Sidebar />
+        </Router>
       </header>
+      <div className="App-body">
+        <h1>This is the material</h1>
+        <div className="redBox">
+          {/* Content specific to each page goes here */}
+          <h2>Page Title</h2>
+          <p>This is the content of the page within the red box.</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,30 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Sidebar.css'
+import Logo from "../Media/CedaTex-Logo.png"
+
 function Sidebar() {
     return (
       <div className='navigation'>
-          <nav className='navbar navbar-default fixed-top'>
+        
+          <nav className='navbar navbar-default'>
               <div className='container-fluid'>
                   <ul className="nav navbar-nav">
+                          <li className='CedaTex'>
+                            <img class="navbar-brand" src={Logo}/>
+                          </li>
                           <li>
                               <Link to="">
-                              <ion-icon name="">Home</ion-icon>
+                              <ion-icon name="home">Home</ion-icon>
                               </Link>
                           </li>
                           <li>
                               <Link to="">
-                              <ion-icon name="">Projects</ion-icon>
+                              <ion-icon name="file-tray-full">Projects</ion-icon>
                               </Link>
                           </li>
                           <li>
                               <Link to="">
-                              <ion-icon name="">About</ion-icon>
+                              <ion-icon name="person">Profile</ion-icon>
                               </Link>
                           </li>
-                          
-                      </ul>
-                  </div>
-              </nav>
+                    </ul>
+
+                </div>
+
+            </nav>
           
-      </div>
+        </div>
     )
   }
   
