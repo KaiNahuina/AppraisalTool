77ebcdf3014721a67b0bf6184aca93d71ae91288
Angel Salinas <a.m.salinas1901@gmail.com>
2024-02-07 15:39:22 +0000

    Project Cards and Information

--- src/MyProjects.js
+++ src/MyProjects.js
@@ -1,9 +1,32 @@
-import React from 'react'
+// ProjectsPage.js
 
-function MyProjects() {
+import React from 'react';
+import ProjectCard from './ProjectCard'; // Import the ProjectCard component
+import './MyProjects.css'; // Import the corresponding CSS file
+
+
+const projects = [
+  {
+    name: 'Project 1',
+    description: 'Description for Project 1',
+    image: 'img/project.jpg',
+  },
+  {
+    name: 'Project 2',
+    description: 'Description for Project 2',
+    image: "img/project.jpg",
+  },
+  // Add more projects as needed
+];
+
+const ProjectsPage = () => {
   return (
-    <div>MyProjects</div>
-  )
-}
+    <div className="projects-container">
+      {projects.map((project, index) => (
+        <ProjectCard key={index} project={project} />
+      ))}
+    </div>
+  );
+};
 
-export default MyProjects
+export default ProjectsPage;
