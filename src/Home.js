823de5bf1c60f22365f2a33a0d853eee570a2962
Angel Salinas <a.m.salinas1901@gmail.com>
2024-02-07 18:06:46 +0000

    Home Page Table

--- src/Home.js
+++ src/Home.js
@@ -3,79 +3,132 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <div class="grid-container">
-        <div class="first-column">
-        <div class="grid-item">Category 1</div>
-        <div class="grid-item">Category 2</div>
-        <div class="grid-item">Category 3</div>
-        <div class="grid-item">Category 4</div>
-        <div class="grid-item">Category 5</div>
-        <div class="grid-item">Category 6</div>
-        <div class="grid-item">Category 7</div>
-      </div>
-      <div class="grid-container">
-        <div class="grid-item">
-          <select id="dropdown1">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
-        </div>
-        <div class="grid-item">
-          <select id="dropdown2">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
-        </div>
-        <div class="grid-item">
-          <select id="dropdown3">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
-        </div>
-        <div class="grid-item">
-          <select id="dropdown4">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
-        </div>
-        <div class="grid-item">
-          <select id="dropdown5">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
-        </div>
-        <div class="grid-item">
-          <select id="dropdown6">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
-        </div>
-        <div class="grid-item">
-          <select id="dropdown7">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
-          </select>
-        </div>
-      </div>
-      
-      <div class="grid-container">
-        <div class="grid-item" id="display1"></div>
-        <div class="grid-item" id="display2"></div>
-        <div class="grid-item" id="display3"></div>
-        <div class="grid-item" id="display4"></div>
-        <div class="grid-item" id="display5"></div>
-        <div class="grid-item" id="display6"></div>
-        <div class="grid-item" id="display7"></div>
-      </div>
-
-    </div>
+      <table class="table">
+      <tr>
+        <th class="columnTitle">Category Name</th>
+        <th class="columnTitle">Selected Values</th>
+        <th class="columnTitle">Points</th>
+      </tr>
+      <tr>
+        <td class="cell">Team Size</td>
+        <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      </tr>
+      <tr>
+        <td class="cell">Geographic Distribution</td>
+        <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      </tr>
+      <tr>
+        <td class="cell">Organizational Distribution</td>
+        <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      </tr>
+      <tr>
+        <td class="cell">Skill Availability</td>
+        <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      </tr>
+      <tr>
+        <td class="cell">Compliance</td>
+        <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      </tr>
+      <tr>
+        <td class="cell">Solution Complexity</td>
+        <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      </tr>
+      <tr>
+        <td class="cell">Domain Complexity</td>
+        <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      <td class="cell">
+        <select>
+          <option value="value1">value1</option>
+          <option value="value2">value2</option>
+          <option value="value3">value3</option>
+        </select>
+      </td>
+      </tr>
+      </table>
     </div>
   )
 }
